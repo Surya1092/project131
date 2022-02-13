b40d461a618e31149a249aedac9f62d21d526b5b
Surya1092 <96676153+Surya1092@users.noreply.github.com>
2022-02-13 13:56:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 img = "";
+objects = [];
+status = "";
+
 
 function preload(){
     img = loadImage('dog_cat.jpg');
@@ -8,12 +11,38 @@ function preload(){
 function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status: detecting objects";
+}
+
+function modelLoaded(){
+    console.log("model is loaded !!!");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 function draw(){
     image(img, 0, 0, 640, 420);
-    fill("#ff0000");
-    text("dog", 45, 75);
-    noFill();
-    rect(30, 60, 450, 350);
+    if (status != "") {
+        for ( i = 0; i<objects.length; i++){
+            document.getElementById("status").innerHTML = "status;object_deteced";
+            fill("#0000ff");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+150);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            
+        } 
+            
+        
+    }
 }
